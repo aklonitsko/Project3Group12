999146fe1799a5f26cf342798eea378184b4f04a
Brian <Brian@Antec-Intel>
2016-11-27 14:14:10 -0800

    Add fields and getter/setter for academic record, student, transfer school

--- src/academic/TransferSchool.java
+++ src/academic/TransferSchool.java
@@ -8,5 +8,36 @@ package academic;
  *
  */
 public class TransferSchool {
+	
+	private String myName;
+	private double myGPA;
+	private String myDegreeEarned;
+	
+	
+	public TransferSchool(String theName, double theGPA, String theDegreeEarned){
+		this.setName(theName);
+		this.setGPA(theGPA);
+		this.setDegreeEarned(theDegreeEarned);
+	}
+	
+	public String getName() {
+		return myName;
+	}
+	public void setName(String myName) {
+		this.myName = myName;
+	}
+	public double getGPA() {
+		return myGPA;
+	}
+	public void setGPA(double myGPA) {
+		this.myGPA = myGPA;
+	}
+	public String getDegreeEarned() {
+		return myDegreeEarned;
+	}
+	public void setDegreeEarned(String myDegreeEarned) {
+		this.myDegreeEarned = myDegreeEarned;
+	}
+	
 
 }
--- src/data/AcademicDB.java
+++ src/data/AcademicDB.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import academic.AcademicRecord;
 import client.Client;
 
 /**
