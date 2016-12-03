1a0e042ad62951f84f0b171fd3c1d6cbedcd6d98
BGibbon1 <BGibbon1@WABELHLP0700160.gsm1900.org>
2016-12-03 14:24:59 -0800

    Removed StudentCollection changes

--- src/student/StudentCollection.java
+++ src/student/StudentCollection.java
@@ -3,37 +3,12 @@
  */
 package student;
 
-import java.util.ArrayList;
-
-import data.StudentDB;
-
 /**
  * @author Andrew,Brandon,Brian
  *
  */
 public class StudentCollection {
 
-	private StudentDB myStudentDB;
-	private ArrayList<Student> myStudentList;
-	
-	public StudentCollection() {
-		
-		myStudentDB = new StudentDB();
-		myStudentList = new ArrayList<Student>();
-	}
-	
-	public boolean addStudent(String theName) {
-		
-		boolean flag = false;
-		
-		try {
-			
-		} catch(Exception e) {
-			
-		}
-	}
-	
-	
 
 
 }
